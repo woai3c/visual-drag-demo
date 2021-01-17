dc74901babcfdfaa654a7abb6bc96db1db886645
woai3c <411020382@qq.com>
2021-01-17 22:45:26 +0800

    refactor(ContextMenu): 使用 v-if 代替 v-show 菜单展示

--- src/components/Editor/ContextMenu.vue
+++ src/components/Editor/ContextMenu.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="contextmenu" v-show="menuShow" :style="{ top: menuTop + 'px', left: menuLeft + 'px' }">
         <ul>
-            <li @click="copy" v-show="curComponent">复制</li>
-            <li @click="paste">粘贴</li>
-            <li @click="cut" v-show="curComponent">剪切</li>
-            <li @click="deleteComponent" v-show="curComponent">删除</li>
-            <li @click="topComponent" v-show="curComponent">置顶</li>
-            <li @click="bottomComponent" v-show="curComponent">置底</li>
-            <li @click="upComponent" v-show="curComponent">上移</li>
-            <li @click="downComponent" v-show="curComponent">下移</li>
+            <template v-if="curComponent">
+                <li @click="copy">复制</li>
+                <li @click="paste">粘贴</li>
+                <li @click="cut">剪切</li>
+                <li @click="deleteComponent">删除</li>
+                <li @click="topComponent">置顶</li>
+                <li @click="bottomComponent">置底</li>
+                <li @click="upComponent">上移</li>
+                <li @click="downComponent">下移</li>
+            </template>
+            <li v-else @click="paste">粘贴</li>
         </ul>
     </div>
 </template>
