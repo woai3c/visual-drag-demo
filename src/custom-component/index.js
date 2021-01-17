d2400cc136a82d1a57f8b447a9ce2c3115b3ff9d
woai3c <411020382@qq.com>
2021-01-17 21:40:14 +0800

    chore: 将自定义组件设为懒加载

--- src/custom-component/index.js
+++ src/custom-component/index.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
-import Picture from './Picture'
-import VText from './VText'
-import VButton from './VButton'
 
-const components = {
-    Picture,
-    VText,
-    VButton,
-}
+const components = [
+    'Picture',
+    'VText',
+    'VButton',
+]
 
-Object.keys(components).forEach(key => {
-    Vue.component(key, components[key])
+components.forEach(key => {
+    Vue.component(key, () => import(`@/custom-component/${key}`))
 })
