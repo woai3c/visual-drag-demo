31d651a479a26040ec4603b93f367c810ba7363d
woai3c <411020382@qq.com>
2021-01-29 15:26:24 +0800

    fix(Shape): 限定 shape 组件的 style 样式范围 (https://github.com/woai3c/visual-drag-demo/issues/5)

--- src/components/Editor/index.vue
+++ src/components/Editor/index.vue
@@ -76,26 +76,14 @@ export default {
         },
 
         getShapeStyle(style) {
-            const result = { ...style }
-            if (result.width) {
-                result.width += 'px'
-            }
-
-            if (result.height) {
-                result.height += 'px'
-            }
-
-            if (result.top) {
-                result.top += 'px'
-            }
-
-            if (result.left) {
-                result.left += 'px'
-            }
-
-            if (result.rotate) {
-                result.transform = 'rotate(' + result.rotate + 'deg)'
-            }
+            const result = {};
+            ['width', 'height', 'top', 'left', 'rotate'].forEach(attr => {
+                if (attr != 'rotate') {
+                    result[attr] = style[attr] + 'px'
+                } else {
+                    result.transform = 'rotate(' + style[attr] + 'deg)'
+                }
+            })
 
             return result
         },
