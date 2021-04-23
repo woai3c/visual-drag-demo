0e7bfe6c38c521b39a528831857f7cbc93321010
woai3c <411020382@qq.com>
2021-04-23 21:33:37 +0800

    ci: 对 commit message 进行验证

---
+++ scripts/verify-commit.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const chalk = require('chalk')
+
+const msgPath = process.env.HUSKY_GIT_PARAMS
+const msg = require('fs')
+.readFileSync(msgPath, 'utf-8')
+.trim()
+
+const commitRE = /^(feat|fix|docs|style|refactor|perf|test|build|ci|chore|release)(\(.+\))?: .{1,50}/
+
+if (!commitRE.test(msg)) {
+    console.log()
+    console.error(
+        `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
+            '不合法的 commit 消息格式',
+        )}\n\n`
+          + chalk.red(
+              '  请使用正确的提交格式:\n\n',
+          )
+          + `    ${chalk.green('feat: add \'comments\' option')}\n`
+          + `    ${chalk.green('fix: handle events on blur (close #28)')}\n\n`
+          + chalk.red('  请查看 git commit 提交规范：https://github.com/woai3c/Front-end-articles/blob/master/git%20commit%20style.md。\n'),
+    )
+
+    process.exit(1)
+}
