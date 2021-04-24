a898cef06981ab66362f63436d295ecd0623fe8e
woai3c <411020382@qq.com>
2021-04-24 10:38:27 +0800

    chore: 矩形添加文字上下对齐方式和边框风格修改

--- src/components/AttrList.vue
+++ src/components/AttrList.vue
@@ -1,3 +1,4 @@
+<!-- TODO: 这个页面后续将用 JSX 重构 -->
 <template>
     <div class="attr-list">
         <el-form>
@@ -5,13 +6,31 @@
                 <el-color-picker v-if="key == 'borderColor'" v-model="curComponent.style[key]"></el-color-picker>
                 <el-color-picker v-else-if="key == 'color'" v-model="curComponent.style[key]"></el-color-picker>
                 <el-color-picker v-else-if="key == 'backgroundColor'" v-model="curComponent.style[key]"></el-color-picker>
-                <el-select v-else-if="key == 'textAlign'" v-model="curComponent.style[key]">
-                    <el-option
-                        v-for="item in options"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value"
-                    ></el-option>
+                <el-select v-else-if="selectKey.includes(key)" v-model="curComponent.style[key]">
+                    <template v-if="key == 'textAlign'">
+                        <el-option
+                            v-for="item in textAlignOptions"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
+                        ></el-option>
+                    </template>
+                    <template v-else-if="key == 'borderStyle'">
+                        <el-option
+                            v-for="item in borderStyleOptions"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
+                        ></el-option>
+                    </template>
+                    <template v-else>
+                        <el-option
+                            v-for="item in verticalAlignOptions"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value"
+                        ></el-option>
+                    </template>
                 </el-select>
                 <el-input type="number" v-else v-model="curComponent.style[key]" />
             </el-form-item>
@@ -27,7 +46,7 @@ export default {
     data() {
         return {
             excludes: ['Picture', 'Group'], // 这些组件不显示内容
-            options: [
+            textAlignOptions: [
                 {
                     label: '左对齐',
                     value: 'left',
@@ -41,6 +60,31 @@ export default {
                     value: 'right',
                 },
             ],
+            borderStyleOptions: [
+                {
+                    label: '实线',
+                    value: 'solid',
+                },
+                {
+                    label: '虚线',
+                    value: 'dashed',
+                },
+            ],
+            verticalAlignOptions: [
+                {
+                    label: '上对齐',
+                    value: 'top',
+                },
+                {
+                    label: '居中对齐',
+                    value: 'middle',
+                },
+                {
+                    label: '下对齐',
+                    value: 'bottom',
+                },
+            ],
+            selectKey: ['textAlign', 'borderStyle', 'verticalAlign'],
             map: {
                 left: 'x 坐标',
                 top: 'y 坐标',
@@ -48,6 +92,7 @@ export default {
                 width: '宽',
                 color: '颜色',
                 backgroundColor: '背景色',
+                borderStyle: '边框风格',
                 borderWidth: '边框宽度',
                 borderColor: '边框颜色',
                 borderRadius: '边框半径',
@@ -55,13 +100,15 @@ export default {
                 fontWeight: '字体粗细',
                 lineHeight: '行高',
                 letterSpacing: '字间距',
-                textAlign: '对齐方式',
                 opacity: '透明度',
+                textAlign: '左右对齐',
+                verticalAlign: '上下对齐',
             },
         }
     },
     computed: {
         styleKeys() {
+            console.log(this.$store.state.curComponent.style)
             return this.$store.state.curComponent? Object.keys(this.$store.state.curComponent.style) : []
         },
         curComponent() {
