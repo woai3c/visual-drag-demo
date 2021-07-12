73a352786ab454b40515945f8236a941023808a9
woai3c <411020382@qq.com>
2021-07-12 22:39:26 +0800

    style: 修改代码格式

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 export function deepCopy(target) {
     if (typeof target == 'object') {
-        const result = Array.isArray(target)? [] : {}
+        const result = Array.isArray(target) ? [] : {}
         for (const key in target) {
             if (typeof target[key] == 'object') {
                 result[key] = deepCopy(target[key])
