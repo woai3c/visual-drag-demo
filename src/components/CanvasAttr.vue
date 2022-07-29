1c0fb6507f1223bdd46384762445aa1d39782bb4
woai3c <411020382@qq.com>
2022-07-29 22:29:08 +0800

    chore: 画布属性面板添加标题

--- src/components/CanvasAttr.vue
+++ src/components/CanvasAttr.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="attr-container">
-        <el-form>
+        <p class="title">画布属性</p>
+        <el-form style="padding: 20px;">
             <el-form-item v-for="(key, index) in Object.keys(options)" :key="index" :label="options[key]">
                 <el-color-picker v-if="isIncludesColor(key)" v-model="canvasStyleData[key]" show-alpha></el-color-picker>
                 <el-input v-else v-model.number="canvasStyleData[key]" type="number" />
@@ -36,6 +37,15 @@ export default {
 
 <style lang="scss">
 .attr-container {
-    padding: 20px;
+    .title {
+        text-align: center;
+        margin-bottom: 10px;
+        height: 40px;
+        line-height: 40px;
+        border-bottom: 2px solid #e4e7ed;
+        font-size: 14px;
+        font-weight: 500;
+        color: #303133;
+    }
 }
 </style>
