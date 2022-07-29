4394fc25a5986483b184762c7ddc4654e10ea72a
woai3c <411020382@qq.com>
2022-07-29 20:19:26 +0800

    feat: 添加实时展示组件列表的功能

---
+++ src/components/RealTimeComponentList.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="real-time-component-list">
+        <div
+            v-for="(item, index) in componentData"
+            :key="index"
+            class="list"
+            :class="{ actived: transformIndex(index) === curComponentIndex }"
+            @click="onClick(transformIndex(index))"
+        >
+            <span class="iconfont" :class="'icon-' + getComponent(index).icon"></span>
+            <span>{{ getComponent(index).label }}</span>
+            <div class="icon-container">
+                <span class="iconfont icon-shangyi" @click="upComponent(transformIndex(index))"></span>
+                <span class="iconfont icon-xiayi" @click="downComponent(transformIndex(index))"></span>
+                <span class="iconfont icon-shanchu" @click="deleteComponent(transformIndex(index))"></span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    computed: mapState([
+        'componentData',
+        'curComponent',
+        'curComponentIndex',
+    ]),
+    methods: {
+        getComponent(index) {
+            return this.componentData[this.componentData.length - 1 - index]
+        },
+
+        transformIndex(index) {
+            return this.componentData.length - 1 - index
+        },
+
+        onClick(index) {
+            this.setCurComponent(index)
+        },
+
+        deleteComponent() {
+            setTimeout(() => {
+                this.$store.commit('deleteComponent')
+                this.$store.commit('recordSnapshot')
+            })
+        },
+
+        upComponent() {
+            setTimeout(() => {
+                this.$store.commit('upComponent')
+                this.$store.commit('recordSnapshot')
+            })
+        },
+
+        downComponent() {
+            setTimeout(() => {
+                this.$store.commit('downComponent')
+                this.$store.commit('recordSnapshot')
+            })
+        },
+
+        setCurComponent(index) {
+            this.$store.commit('setCurComponent', { component: this.componentData[index], index })
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.real-time-component-list {
+    height: 35%;
+
+    .list {
+        height: 30px;
+        cursor: grab;
+        text-align: center;
+        color: #333;
+        display: flex;
+        align-items: center;
+        font-size: 12px;
+        padding: 0 10px;
+        position: relative;
+        user-select: none;
+
+        &:active {
+            cursor: grabbing;
+        }
+
+        &:hover {
+            background-color: rgba(200, 200, 200, .2);
+
+            .icon-container {
+                display: block;
+            }
+        }
+
+        .iconfont {
+            margin-right: 4px;
+            font-size: 16px;
+        }
+
+        .icon-wenben,
+        .icon-tupian {
+            font-size: 14px;
+        }
+
+        .icon-container {
+            position: absolute;
+            right: 10px;
+            display: none;
+
+            .iconfont {
+                cursor: pointer;
+            }
+        }
+    }
+
+    .actived {
+        background: rgb(218, 235, 254) !important;
+    }
+}
+</style>
--- src/store/layer.js
+++ src/store/layer.js
@@ -3,39 +3,47 @@ import toast from '@/utils/toast'
 
 export default {
     mutations: {
-        upComponent({ componentData, curComponentIndex }) {
+        upComponent(state) {
+            const { componentData, curComponentIndex } = state
             // 上移图层 index，表示元素在数组中越往后
             if (curComponentIndex < componentData.length - 1) {
                 swap(componentData, curComponentIndex, curComponentIndex + 1)
+                state.curComponentIndex = curComponentIndex + 1
             } else {
                 toast('已经到顶了')
             }
         },
 
-        downComponent({ componentData, curComponentIndex }) {
+        downComponent(state) {
+            const { componentData, curComponentIndex } = state
             // 下移图层 index，表示元素在数组中越往前
             if (curComponentIndex > 0) {
                 swap(componentData, curComponentIndex, curComponentIndex - 1)
+                state.curComponentIndex = curComponentIndex - 1
             } else {
                 toast('已经到底了')
             }
         },
 
-        topComponent({ componentData, curComponentIndex, curComponent }) {
+        topComponent(state) {
+            const { componentData, curComponentIndex, curComponent } = state
             // 置顶
             if (curComponentIndex < componentData.length - 1) {
                 componentData.splice(curComponentIndex, 1)
                 componentData.push(curComponent)
+                state.curComponentIndex = componentData.length - 1
             } else {
                 toast('已经到顶了')
             }
         },
 
-        bottomComponent({ componentData, curComponentIndex, curComponent }) {
+        bottomComponent(state) {
+            const { componentData, curComponentIndex, curComponent } = state
             // 置底
             if (curComponentIndex > 0) {
                 componentData.splice(curComponentIndex, 1)
                 componentData.unshift(curComponent)
+                state.curComponentIndex = 0
             } else {
                 toast('已经到底了')
             }
