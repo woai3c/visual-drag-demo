51151616ee6693af360964b54dda88730345051b
woai3c <411020382@qq.com>
2022-07-30 17:56:01 +0800

    chore: 通用样式添加折叠功能，优化用户体验

--- src/custom-component/common/CommonAttr.vue
+++ src/custom-component/common/CommonAttr.vue
@@ -1,18 +1,24 @@
 <template>
-    <el-form>
-        <el-form-item v-for="({ key, label }, index) in styleKeys" :key="index" :label="label">
-            <el-color-picker v-if="isIncludesColor(key)" v-model="curComponent.style[key]" show-alpha></el-color-picker>
-            <el-select v-else-if="selectKey.includes(key)" v-model="curComponent.style[key]">
-                <el-option
-                    v-for="item in optionMap[key]"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value"
-                ></el-option>
-            </el-select>
-            <el-input v-else v-model.number="curComponent.style[key]" type="number" />
-        </el-form-item>
-    </el-form>
+    <div style="margin-bottom: 18px;">
+        <el-collapse v-model="activeName" accordion>
+            <el-collapse-item title="通用样式" name="common">
+                <el-form style="padding: 10px; padding-top: 0;">
+                    <el-form-item v-for="({ key, label }, index) in styleKeys" :key="index" :label="label">
+                        <el-color-picker v-if="isIncludesColor(key)" v-model="curComponent.style[key]" show-alpha></el-color-picker>
+                        <el-select v-else-if="selectKey.includes(key)" v-model="curComponent.style[key]">
+                            <el-option
+                                v-for="item in optionMap[key]"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value"
+                            ></el-option>
+                        </el-select>
+                        <el-input v-else v-model.number="curComponent.style[key]" type="number" />
+                    </el-form-item>
+                </el-form>
+            </el-collapse-item>
+        </el-collapse>
+    </div>
 </template>
 
 <script>
@@ -27,6 +33,7 @@ export default {
             borderStyleOptions,
             verticalAlignOptions,
             selectKey,
+            activeName: 'common',
         }
     },
     computed: {
