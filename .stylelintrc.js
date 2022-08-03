107e94809753ff9d341a568a6c9f7bb8559ce9ea
woai3c <411020382@qq.com>
2022-08-04 00:13:29 +0800

    build: 更新依赖

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,25 @@
 module.exports = {
-    extends: 'stylelint-config-standard',
+    extends: [
+        'stylelint-config-standard-scss',
+        'stylelint-config-recommended-vue'
+    ],
+    customSyntax: 'postcss-html',
+    overrides: [
+        {
+            files: ['**/*.{scss,css,sass}'],
+            customSyntax: 'postcss-scss'
+        },
+    ],
     rules: {
-        'indentation': 4,
+        indentation: 4,
+        'selector-class-pattern': ['.*'],
+        'alpha-value-notation': 'number',
+        'color-function-notation': 'legacy',
         'selector-pseudo-element-no-unknown': [
             true,
             {
-                ignorePseudoElements: ['v-deep']
-            }
+                ignorePseudoElements: ['v-deep'],
+            },
 
         ],
         'number-leading-zero': 'never',
@@ -15,7 +28,7 @@ module.exports = {
         'selector-type-no-unknown': null,
         'at-rule-no-unknown': null,
         'no-duplicate-selectors': null,
-        'no-empty-source':null,
-        'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global', 'deep'] }]
-    }
+        'no-empty-source': null,
+        'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global', 'deep'] }],
+    },
 }
