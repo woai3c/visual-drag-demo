be31a25d93fb38bae48cfcd7cc0711cc1cde3903
woai3c <411020382@qq.com>
2022-11-02 22:57:54 +0800

    style: 代码格式化

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -43,7 +43,7 @@
 
         <!-- 预览 -->
         <Preview v-if="isShowPreview" :is-screenshot="isScreenshot" @close="handlePreviewChange" />
-        <AceEditor v-if="isShowAceEditor" @closeEditor="closeEditor"/>
+        <AceEditor v-if="isShowAceEditor" @closeEditor="closeEditor" />
     </div>
 </template>
 
