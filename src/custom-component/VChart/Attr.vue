c044a3ff97ceaede3f2829d62fab7b672314370e
leoyongyuan <1359195795@qq.com>
2022-11-25 23:27:15 +0800

    refactor: 在图表组件中用数据修改面板更换为dialog，统一风格，删除winbox #104

--- src/custom-component/VChart/Attr.vue
+++ src/custom-component/VChart/Attr.vue
@@ -46,14 +46,16 @@
                 <el-button @click="openStaticWinbox">修改数据</el-button>
             </el-form-item>
         </el-form>
-        <div style="display: none">
-            <div id="staticData">
-                <div class="ace" ref="ace"></div>
-                <el-button class="btn" @click="updatedata">
-                    更新数据
-                </el-button>
-            </div>
-        </div>
+
+        <el-dialog
+            title="数据修改"
+            :visible.sync="dialogVisible"
+            width="75%">
+            <div class="ace" ref="ace"></div>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="updatedata">更新数据</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -61,13 +63,13 @@
 import ace from 'ace-builds'
 import 'ace-builds/src-min-noconflict/theme-chrome'
 import 'ace-builds/src-min-noconflict/mode-json5'
-import WinBox from 'winbox/src/js/winbox'
 import CommonAttr from '@/custom-component/common/CommonAttr.vue'
 
 export default {
     components: { CommonAttr },
     data() {
         return {
+            dialogVisible: false,
             charts: [
                 {
                     title: 'bar',
@@ -84,25 +86,6 @@ export default {
             ],
         }
     },
-    mounted() {
-        ace.config.set('basePath', 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.14/')
-        this.editor = ace.edit(this.$refs.ace, {
-            maxLines: 28,
-            minLines: 28,
-            fontSize: 14,
-            theme: 'ace/theme/chrome',
-            mode: 'ace/mode/json5',
-            tabSize: 4,
-            readOnly: false,
-            enableBasicAutocompletion: true,
-            enableLiveAutocompletion: true,
-            enableSnippets: true,
-        })
-        // 初始化图表数据在editor中
-        let data = JSON.stringify(this.curComponent.propValue.option.series.data)
-        let xAxis = JSON.stringify(this.curComponent.propValue.option.xAxis.data)
-        this.editor.setValue(data + '\n' + xAxis)
-    },
     computed: {
         curComponent() {
             return this.$store.state.curComponent
@@ -112,19 +95,29 @@ export default {
         },
     },
     methods: {
-        // 打开一个Winbox弹出框
         openStaticWinbox() {
-            new WinBox('Static Data', {
-                background: '#04A9F5',
-                x: 'center',
-                y: 'center',
-                width: '75%',
-                height: '75%',
-                class: 'modern',
-                mount: document.getElementById('staticData'),
+            this.dialogVisible = !this.dialogVisible
+            this.$nextTick(() => {
+                ace.config.set('basePath', 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.14/')
+                this.editor = ace.edit(this.$refs.ace, {
+                    maxLines: 28,
+                    minLines: 28,
+                    fontSize: 14,
+                    theme: 'ace/theme/chrome',
+                    mode: 'ace/mode/json5',
+                    tabSize: 4,
+                    readOnly: false,
+                    enableBasicAutocompletion: true,
+                    enableLiveAutocompletion: true,
+                    enableSnippets: true,
+                })
+                // 初始化图表数据在editor中
+                let data = JSON.stringify(this.curComponent.propValue.option.series.data)
+                let xAxis = JSON.stringify(this.curComponent.propValue.option.xAxis.data)
+                this.editor.setValue(data + '\n' + xAxis)
             })
         },
-        
+
         // 寻找数组[]
         findstring(str, ch1, ch2) { 
             return str.substr(str.indexOf(ch1), str.indexOf(ch2) + 1)
@@ -137,6 +130,8 @@ export default {
             let ArrXAxis = this.findstring(str.substr(str.indexOf(']') + 1), '[', ']')
             this.curComponent.propValue.option.series.data = JSON.parse(Arrdata)
             this.curComponent.propValue.option.xAxis.data = JSON.parse(ArrXAxis)
+            this.$message.success('更新成功')
+            this.dialogVisible = !this.dialogVisible
         },
 
         // 更换表格类型
@@ -147,14 +142,11 @@ export default {
 }
 </script>
 <style>
-@import '@/styles/winbox.css';
 .ace {
     margin: 5px;
-}
-.btn {
-    float: right;
-    margin-right: 10px;
-    margin-top: 2px;
+    margin-top: -20px;
+    height: 600px;
+    width: 1100px;
 }
 .attr {
     margin: 13px;
