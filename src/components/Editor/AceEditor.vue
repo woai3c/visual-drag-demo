af7008500a601e192815bcd3839b5d01b0e0c1f5
woai3c <411020382@qq.com>
2024-02-23 15:51:24 +0800

    feat: 添加导入导出功能

--- src/components/Editor/AceEditor.vue
+++ src/components/Editor/AceEditor.vue
@@ -1,38 +1,38 @@
 <template>
     <div class="ace">
-        <el-button
-            class="btn"
-            style="margin-left: 380px;"
-            icon="el-icon-search"
-            @click="openSearchBox"
-        >
-            查找
-        </el-button>
-        <el-button
-            class="btn"
-            style="margin-left: 10px;"
-            icon="el-icon-close"
-            @click="closeEditor"
-        >
-            关闭
-        </el-button>
+        <div class="header">
+            <el-button
+                class="btn"
+                icon="el-icon-search"
+                @click="openSearchBox"
+            >
+                查找
+            </el-button>
+            <el-button
+                class="btn"
+                icon="el-icon-close"
+                @click="closeEditor"
+            >
+                关闭
+            </el-button>
+        </div>
         <div class="ace-editor">
             <div ref="ace" class="editor" />
         </div>
-        <el-button
-            type="primary"
-            style="margin-left: 360px;"
-            @click="setCode"
-        >
-            重置代码
-        </el-button>
-        <el-button
-            type="success"
-            style="margin-left: 20px;"
-            @click="getCode"
-        >
-            保存提交
-        </el-button>
+        <div class="footer">
+            <el-button
+                type="primary"
+                @click="setCode"
+            >
+                重置代码
+            </el-button>
+            <el-button
+                type="success"
+                @click="getCode"
+            >
+                保存提交
+            </el-button>
+        </div>
     </div>
 </template>
 
@@ -67,8 +67,8 @@ export default {
     mounted() {
         ace.config.set('basePath', 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.14/')
         this.editor = ace.edit(this.$refs.ace, {
-            maxLines: 34,
-            minLines: 34,
+            maxLines: 40,
+            minLines: 40,
             fontSize: 14,
             theme: 'ace/theme/one_dark',
             mode: 'ace/mode/json5',
@@ -112,7 +112,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .ace {
     position: absolute;
     height: calc(100% - 80px);
@@ -121,13 +121,21 @@ export default {
     background-color: #fff;
     border: 1px solid #ddd;
     z-index: 100;
+    padding: 10px;
+
+    .header,
+    .footer {
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    .ace-editor {
+        height: calc(100% - 80px);
+        overflow: auto;
+    }
 }
 
 .editor {
-    margin: 10px 5px;
-}
-
-.btn {
-    margin-top: 5px;
+    margin: 10px 0;
 }
 </style>
